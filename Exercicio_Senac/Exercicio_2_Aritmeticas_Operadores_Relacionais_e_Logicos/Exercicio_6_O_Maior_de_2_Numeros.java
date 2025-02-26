ff56aac625d70e357f399cd711bc069e81b44eda
João Batista <joaobatistawff@gmail.com>
2025-02-26 19:26:34 -0300

    Update Exercicio_6_O_Maior_de_2_Numeros.java

--- Exercicio_Senac/Exercicio_2_Aritmeticas_Operadores_Relacionais_e_Logicos/Exercicio_6_O_Maior_de_2_Numeros.java
+++ Exercicio_Senac/Exercicio_2_Aritmeticas_Operadores_Relacionais_e_Logicos/Exercicio_6_O_Maior_de_2_Numeros.java
@@ -18,8 +18,10 @@ public class Exercicio_6_O_Maior_de_2_Numeros {
         System.out.println("Digite o segundo numero");
         double n2 = tecla.nextDouble();    
     boolean a = (n1>n2);
-    if (a==true){
+   if (a==true){
         System.out.println("O numero "+n1+" e maior que "+n2);
+    }else if (n1==n2){
+    System.out.println("Os numeros sao iguais");
     }else{
         System.out.println("O numero "+n2+" e maior que "+n1);    
     }
