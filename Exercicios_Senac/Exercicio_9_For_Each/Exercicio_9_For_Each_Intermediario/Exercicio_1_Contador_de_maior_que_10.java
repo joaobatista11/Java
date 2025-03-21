a0a61245aad6c9080aaa679091d82e05adca2568
João Batista <joaobatistawff@gmail.com>
2025-03-20 21:48:16 -0300

    20/03

---
+++ Exercicios_Senac/Exercicio_9_For_Each/Exercicio_9_For_Each_Intermediario/Exercicio_1_Contador_de_maior_que_10.java
@@ -0,0 +1,25 @@
+
+package Exercicio_9_For_Each_Intermediario;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Exercicio_1_Contador_de_maior_que_10 {
+    public static void main(String[] args) {
+        Scanner tecla = new Scanner(System.in);
+        HashSet <Integer> set = new HashSet<>();
+        
+        int n1;
+        for (int i = 1; i <=5; i++) {
+            System.out.println((i) + ". Digite um numero");
+            int[] n2 = new int [tecla.nextInt()];           
+            if(n2.length>10){
+            set.add(n2.length);
+            }
+        }
+        System.out.println("\nNumeros maiores que 10\n");
+        for(int lista: set){
+        System.out.println(lista);
+        }
+}
+}
